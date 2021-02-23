dabde8f4a90da4748864e313eec7469a598f951b
YUZAMIN <untmd.crmn@gmail.com>
2021-02-23 22:51:44 +0900

    1d_4c

--- src/App.js
+++ src/App.js
@@ -1,88 +1,74 @@
-import React from "react";
+import React, {useState} from "react";
 
 import './App.css';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      gender: "",
-      name: "",
-    };
-  }
+export default function App() {
+  const [name, setName] = useState("");
+  const [gender, setGender] = useState("");
 
-  handleNameChange = event => {
-    this.setState({name: event.target.value});
+const handleNameChange = event => {
+    setName(event.target.value);
   }
 
 
-  handleGenderChange = event => {
-    this.setState({
-      gender: event.target.value
-    });
+const handleGenderChange = event => {
+    setGender(event.target.value);
   };
 
-  handleSubmit = () => {
+const handleSubmit = () => {
     alert("submitted!");
   }
 
 
 
-  render() {
-    return (
-      <div>
+  return (
+    <div>
 
-        <div>
-          <h1>직업 가치관 검사</h1>
-            <form> 
-              <label>
-                Name:
-                  <input type="text" value={this.state.value} onChange={this.handleNameChange} />
-              </label>
-            </form>
-        </div>
-
-        <div className="form-check">
+    <div>
+      <h1>직업 가치관 검사</h1>
+        <form> 
+          <label>
+            Name:
+              <input type="text" name="name" value={name} onChange={handleNameChange} />
+          </label>
+          <br />
           <label>
             <input
               type="radio"
-              name="react-tips"
-              value="woman"
-              checked={this.state.gender === "woman"}
-              onChange={this.handleGenderChange}
+              name="gender"
+              value="100324"
+              onChange={handleGenderChange}
               className="form-check-input"
             />
             여성
           </label>
-        </div>
-
-        <div className="form-check">
           <label>
             <input
               type="radio"
-              name="react-tips"
-              value="man"
-              checked={this.state.gender === "man"}
-              onChange={this.handleGenderChange}
+              name="gender"
+              value="100323"
+              onChange={handleGenderChange}
               className="form-check-input"
             />
             남성
           </label>
+
+        </form>
+    </div>
+    <div>
+          <h1> 성별: {gender}</h1>
+          <h3> 이름: {name}</h3>
         </div>
 
         <div>
-          <h1> 성별: {this.state.gender}</h1>
-          <h3> 이름: {this.state.name}</h3>
-        </div>
-
-        <div>
-          <button type="submit" disabled={this.state.gender.length < 1 || this.state.name.length < 1} onClick={this.handleSubmit} >
+          <button type="submit" disabled={!gender || !name} onClick={handleSubmit} >
             검사 시작
           </button>
         </div>
 
       </div>
-    );    
-  }
+  );
 }
-export default App;
+
+
+

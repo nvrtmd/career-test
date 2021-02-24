22c7b2cb7f8db59a43c797594892700d3cad1f38
YUZAMIN <untmd.crmn@gmail.com>
2021-02-24 18:20:53 +0900

    2d_2c

--- src/components/Test.js
+++ src/components/Test.js
@@ -2,59 +2,6 @@ import axios from "axios";
 import React, {useState, useEffect} from "react";
 import { Link } from 'react-router-dom'
 
-// function TestShow() {
-//     return(
-//         <div>
-//             <form>
-//                 <div>
-//                 {content.question}
-//                 </div>
-
-//                 <label>
-//                     <input type="radio" name="q1"></input>
-//                     {content.answer01}
-//                 </label>
-//                 <label>
-//                     <input type="radio" name="q1"></input>
-//                     {content.answer02}
-//                 </label>
-//             </form>
-//         </div>
-//     )
-// }
-
-
-
-
-// function arrayList() {
-    // const [contents, setContents] = useState("");
-
-    // useEffect(() => {
-    //     async function contents(){
-    //         const response = await axios.get(
-    //             `http://www.career.go.kr/inspct/openapi/test/questions?apikey=ad5bf9f6a1f7c1af90eff9aed50ee117&q=6`
-    //         );
-    //         // console.log(response.data.RESULT)
-    //         setContents(response.data.RESULT);
-    //     }
-    //     contents();
-    // }, [])
-
-//     const mapToComponent = data => {
-//         return data.map((content, qitemNo) => {
-//             return (
-//                 <TestShow content={content} key={qitemNo} />
-//             );
-//         })
-//     }
-//     return (
-//         <div>
-//             {mapToComponent(propscontents)}
-//         </div>
-//     )
-// }
-
-
 function Test() {
     const [contents, setContents] = useState("");
 
@@ -79,16 +26,6 @@ function Test() {
         answ01Arr.push(array[i].answer01)
         answ02Arr.push(array[i].answer02)
     }
-    // console.log(questionArr);
-    // console.log(answ01Arr);
-    // console.log(answ02Arr);
-
-    // const handleAnswerChange = e => {
-        
-    // }
-
-
-
     return(
         <div>
             <h1>TEST</h1>
